8f19f2efdb93a5f6b46a7fa5077e067b3cba9648
kapigames <kapigamesdev@gmail.com>
2022-10-09 22:41:47 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+C++ mesonbuild template
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+
+int main(int argc, const char *argv[]) {
+    (void)argc;
+    (void)argv;
+
+    std::printf("Hello world!\n\n");
+
+    return 0;
+}
